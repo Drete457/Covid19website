23b3f2a42811c4017140425b65f9383bd16a9c22
Filipe Mota <drete457@gmail.com>
2020-07-13 21:50:30 +0100

    limit in the map done

--- src/Components/map.jsx
+++ src/Components/map.jsx
@@ -7,7 +7,7 @@ import "../Css/map.css";
 export default function MapView(props) {
   let [countries, setCountries] = useState("");
   let [newColor, setNewColor] = useState("");
-
+  
   useEffect(() => {
     setCountries(props.countries);
     setNewColor(Color(Object.values(props.type).join("")));
@@ -15,7 +15,7 @@ export default function MapView(props) {
 
   return (
     <div>
-      <Map id="mapview" className="mapview" center={[38.7071, -9.13549]} zoom={3} maxZoom={30} >
+      <Map id="mapview" className="mapview" center={[38.7071, -9.13549]} zoom={3} minZoom={2.2} maxZoom={18} maxBounds={[[-89.98155760646617, -180],[89.99346179538875, 180]]} >
         <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
         {Array.from(countries).map((country, index) => {
           return (

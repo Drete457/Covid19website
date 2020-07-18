47240c30baaaa3cb5b79e764e80a047a8c3a0c0a
Filipe Mota <drete457@gmail.com>
2020-07-18 20:15:28 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Covid19website/precache-manifest.f162f947d3e189bf9ec1f436f590a612.js"
+  "/Covid19website/precache-manifest.7b75f8dadf170967de718ad51f7de4b3.js"
 );
 
 self.addEventListener('message', (event) => {

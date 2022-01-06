403fe3ff22423b96db2a3a7052f8773c3f0f345f
Filipe Mota <nada.geral@gmail.com>
2022-01-06 16:28:16 +0000

    fix useHistory

--- src/Functions/backbutton.js
+++ src/Functions/backbutton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useHistory, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import update from "../Controller/update";
 
 export default function BackButton() {
